a84694315cdbe086030389b5e28e593a868b02da
Rubil <mogererubil@gmail.com>
2025-03-11 10:39:55 +0300

    new commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = "John Doe"; // Replace with any name
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
