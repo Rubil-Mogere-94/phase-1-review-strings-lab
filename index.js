4a45f7815ce92d4cd98f11530e5019c40845d3d7
Rubil <mogererubil@gmail.com>
2025-06-03 10:46:39 +0300

    new commit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+// Define currentUser with your name or any string
+let currentUser = 'Alice';
+
+// welcomeMessage should contain "Welcome to Flatbook, " and the value of currentUser, ending with an exclamation point
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// excitedWelcomeMessage should contain "WELCOME TO FLATBOOK, " and the value of currentUser, in uppercase, ending with an exclamation point
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+// shortGreeting should contain "Welcome, " and the first initial of the currentUser, ending with an exclamation mark
+let shortGreeting = `Welcome, ${currentUser[0]}!`;
